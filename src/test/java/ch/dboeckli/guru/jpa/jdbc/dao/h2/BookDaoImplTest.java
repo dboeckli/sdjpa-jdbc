a4918c5c6160b5d948dfefa8946fe4daa77dd4e4
Dominique Boeckli <dboeckli@gmail.com>
2025-11-26 22:38:31 +0100

    fixup! upgrade spring boot 4

--- src/test/java/ch/dboeckli/guru/jpa/jdbc/dao/h2/BookDaoImplTest.java
+++ src/test/java/ch/dboeckli/guru/jpa/jdbc/dao/h2/BookDaoImplTest.java
@@ -6,8 +6,8 @@ import ch.dboeckli.guru.jpa.jdbc.dao.BookDaoImpl;
 import ch.dboeckli.guru.jpa.jdbc.domain.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
